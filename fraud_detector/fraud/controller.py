8369bbf9bbe58c8a172eec72ff8483e8bb797fd4
davidpd7 <david.5697.9@gmail.com>
2024-01-24 17:53:41 +0000

    commit 24012024 1753

--- fraud_detector/fraud/controller.py
+++ fraud_detector/fraud/controller.py
@@ -12,11 +12,13 @@ class Controller:
         self.buttons = self.__view.get_buttons()
         self.bar_buttons = self.__view.get_bar_buttons()
         self.bar_connetions()
+        self.buttons_connections()
 
     def bar_connetions(self):
         self.bar_buttons["expmenu"].triggered.connect(self.__model.export_template)
         self.filename = self.bar_buttons["impmenu"].triggered.connect(self.__model.browse_template)
     
     def buttons_connections(self):
-        pass    
+        self.buttons['push_buttons1'].clicked.connect(self.__model.predictor)
+        self.buttons['push_buttons2'].clicked.connect(self.__model.get_data_processed)
     
--- fraud_detector/fraud/models.py
+++ fraud_detector/fraud/models.py
@@ -1,12 +1,11 @@
 import os
-import re
-from time import sleep
+
 
 from PyQt6.QtWidgets import QFileDialog
 from PyQt6.QtCore import QStandardPaths
 import pandas as pd
 import numpy as np
-from datetime import datetime
+import joblib
 
 from fraud.config import cfg_item
 
@@ -14,47 +13,58 @@ class Model:
 
     def __init__(self):
 
-        self.initial_text = cfg_item("app", "initial_text")
-        self.sleep_time = 1
-        self.selection_text = cfg_item("app", "selection_text")
-        self.error_text = cfg_item("app", "error_text")
-        self.browse_text = cfg_item("app", "browse_text")
-        self.template_columns = [cfg_item("template", "columns")]
-        self.ready_text = cfg_item("template", "ready_text")
-        
-    def welcome(self):
-        print(self.initial_text)
-        sleep(self.sleep_time)
-        self.run()
+        self.template_columns_cat = cfg_item("template", "columns","categorical_cols")
+        self.template_columns_con = cfg_item("template", "columns","numerical_cols")
+        self.ready_text = cfg_item("template", "ready_text")        
+        self.__running = False
 
-    def run(self):
-        
-        print(self.selection_text)
-        answer = str.lower(input())
-        if answer == "y":
-            self.export_template()
-        elif answer == "n":
-            print(self.browse_text)
-            self.run()
-        elif answer == "s":
-            pass
-        else:
-            print(self.error_text)
-            self.run()
-
-
-    def get_data(self):
-        pass
-
-    def stop(self):
-        pass
-
+    def __get_data(self):
+        if self.__running is True:
+            data = pd.read_csv(self.fileName, index_col=0)
+            return data
+    
     def export_template(self):
-        
-        template = pd.DataFrame(columns = self.template_columns)
+        template = pd.DataFrame(columns = self.template_columns_cat + self.template_columns_con)
         template.to_csv(cfg_item("template","name"))
 
     def browse_template(self):
-        self.fileName, _ = QFileDialog.getOpenFileNames(None, 'Open File')
-        return self.fileName
- 
+        self.__running = True
+        self.fileName, _ = QFileDialog.getOpenFileName(None, 'Open File')
+
+    def predictor(self):
+        self.lg_model = joblib.load(os.path.join(*cfg_item("app","model_path")))
+        print(self.lg_model.predict(self.data_processed))
+
+    def get_data_processed(self):
+        
+        self.data_processed = self.__pipeline()
+
+    def __time_variables(self):
+        data = self.__get_data()
+        data['trasaction_hour'] = pd.to_datetime(data['trans_date_trans_time']).dt.hour
+        data['trasaction_day'] = pd.to_datetime(data['trans_date_trans_time']).dt.dayofweek
+        data['dob'] = pd.to_datetime(data['dob'])
+        data['trans_date_trans_time'] = pd.to_datetime(data['trans_date_trans_time'])
+        data['age'] = (data['trans_date_trans_time'] - data['dob']).dt.days // 365
+        return data
+    
+    def __optimal_binning(self):
+
+        data = self.__time_variables()
+        opt_age = joblib.load(os.path.join(*cfg_item("app","binning_age")))
+        opt_amt = joblib.load(os.path.join(*cfg_item("app","binning_amt")))
+        data['amt_binned'] = opt_amt.transform(data["amt"], metric='bins')
+        data['age_binned'] = opt_age.transform(data["age"], metric='bins')
+        return data
+    
+    def __pipeline (self):
+
+        numerical_cols = ['lat', 'long', 'city_pop', 'unix_time', 'merch_lat', 'merch_long',"trasaction_hour"]
+        categorical_cols = ['gender', 'category', 'age_binned', "amt_binned", "trasaction_day"]
+        data = self.__optimal_binning()
+        preprocessor = joblib.load(os.path.join(*cfg_item("app","preprocesor")))
+        data_processed =  preprocessor.transform(data[numerical_cols + categorical_cols])
+        
+        return data_processed
+        
+    
