831260642be62fee02b0c459de4e4d631f3b6b92
David <119945403+davidpd7@users.noreply.github.com>
2024-01-26 17:36:25 +0000

    commit 26012024 1736

--- fraud_detector/fraud/models.py
+++ fraud_detector/fraud/models.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 from PyQt6.QtWidgets import QFileDialog
 import pandas as pd
 import numpy as np
@@ -11,82 +11,105 @@ class Model:
 
     def __init__(self):
 
-        self.template_mandatory_columns = cfg_item("template", "columns","mandatory_columns")
-        self.template_non_mandatory_columns = cfg_item("template", "columns","non_mandatory_columns") 
+        self.__mandatory_columns = cfg_item("template", "columns","mandatory_columns")
+        self.__non_mandatory_columns = cfg_item("template", "columns","non_mandatory_columns") 
         self.__running = False
-        self.__run()
-    
-    def __run(self):
- 
-        self.__load_packages()
-
+        self.__opt_age = joblib.load(os.path.join(*cfg_item("app","packages", "binning_age")))
+        self.__opt_amt = joblib.load(os.path.join(*cfg_item("app","packages","binning_amt")))
+        self.__preprocessor = joblib.load(os.path.join(*cfg_item("app","packages","preprocesor")))
+        self.__lg_model = joblib.load(os.path.join(*cfg_item("app","packages","model_path")))
+        self.__mandatory_indicator = cfg_item("app", "mandatory_indicator")
+  
+    def __columns_check(self, data):
+        if any(data.columns.str.contains(re.escape(self.__mandatory_indicator))):
+            data.columns = data.columns.str.replace(re.escape(self.__mandatory_indicator), "")
+        return True
+       
     def export_template(self):
-
-        template = pd.DataFrame(columns = self.template_mandatory_columns + self.template_non_mandatory_columns)
+        mandatory_columns = [i + self.__mandatory_indicator for i in self.__mandatory_columns]
+        template = pd.DataFrame(columns = mandatory_columns + self.__non_mandatory_columns)
         return template.to_csv(cfg_item("template","name"))
 
     def browse_template(self):
 
         self.__running = True
-        self.fileName, _ = QFileDialog.getOpenFileName(None, 'Open File')
-  
+        self.__fileName, _ = QFileDialog.getOpenFileName(None, "Open File")
+
     def __get_data(self):
+
         if self.__running:
-            data = pd.read_csv(self.fileName, index_col=0)
-            if any(data.columns.str.contains(r"\(*\)")):
-               data.columns = data.columns.str.replace("(*)","")
-            return data
-
-    def predictor(self):
-
-        predictions = self.__lg_model.predict(self.__data_processed)
-        print(predictions)
-        return predictions
+            data = pd.read_csv(self.__fileName, index_col=0)
+            if self.__columns_check(data):
+                if not data.empty:
+                    return data
+        else:
+            return None
 
     def get_data_processed(self):
-        
-        self.__data_processed = self.__pipeline()
-
-    def __time_variables(self):
-
         data = self.__get_data()
-        if data.empty:
-            assert False
+        if data is None:
+            return data
+        if self.__datetime_check(data):
+            data = self.__time_variables(data)
+            data = self.__optimal_binning(data)
+
+            self.__data_processed = self.__pipeline(data)
+            return self.__data_processed
+    
+    def __datetime_check(self,data):
+        date_time_columns = cfg_item("template", "columns","datetime_columns")
         try:
-            data['trasaction_hour'] = pd.to_datetime(data['trans_date_trans_time']).dt.hour
-            data['trasaction_day'] = pd.to_datetime(data['trans_date_trans_time']).dt.dayofweek
-            data['dob'] = pd.to_datetime(data['dob'])
-            data['trans_date_trans_time'] = pd.to_datetime(data['trans_date_trans_time'])
-            data['age'] = (data['trans_date_trans_time'] - data['dob']).dt.days // 365
+            for column in date_time_columns:
+                data =  pd.to_datetime(data[column])
+            return True
+        except:
+           print("Hello")
+
+    def __time_variables(self, data):
+        
+        datetime_column = "trans_date_trans_time"
+        date_of_birth_column = "dob"
+        transaction_hour_column = "trasaction_hour"
+        transaction_day_column ="trasaction_day"
+        transaction_age = "age"
+        number_days_year = 365
+
+        try:
+            data[transaction_hour_column] =  data[datetime_column].dt.hour
+            data[transaction_day_column] = data[datetime_column].dt.dayofweek
+            data[transaction_age] = (data[datetime_column] - data[date_of_birth_column]).dt.days // number_days_year
             return data
         except:
             print("Error 1")
+        
     
-    def __load_packages(self):
+    def __optimal_binning(self, data):
 
-        self.__opt_age = joblib.load(os.path.join(*cfg_item("app","packages", "binning_age")))
-        self.__opt_amt = joblib.load(os.path.join(*cfg_item("app","packages","binning_amt")))
-        self.__preprocessor = joblib.load(os.path.join(*cfg_item("app","packages","preprocesor")))
-        self.__lg_model = joblib.load(os.path.join(*cfg_item("app","packages","model_path")))
-    
-    def __optimal_binning(self):
         try:
-            data = self.__time_variables()
-            data['amt_binned'] = self.__opt_amt.transform(data["amt"], metric='bins')
-            data['age_binned'] = self.__opt_age.transform(data["age"], metric='bins')
+            data["amt_binned"] = self.__opt_amt.transform(data["amt"], metric="bins")
+            data["age_binned"] = self.__opt_age.transform(data["age"], metric="bins")
             return data
         except:
             print("Error 2")
     
-    def __pipeline (self):
+    def __pipeline (self, data):
+
         try:
             numerical_cols = cfg_item("transformed_data","columns", "numerical_cols")
             categorical_cols = cfg_item("transformed_data","columns", "categorical_cols")
-            data = self.__optimal_binning()
             data_processed =  self.__preprocessor.transform(data[numerical_cols + categorical_cols])
             return data_processed
         except:
-            "Error 3"
+            print("Error 3")
+
+    def predictor(self):
+
+        self.__data_processed = self.get_data_processed()
+        if self.__data_processed is None:
+            return None
+        predictions = self.__lg_model.predict(self.__data_processed)
+        print(predictions)
+        return predictions
 
     def __model_metrics(self):
         pass
